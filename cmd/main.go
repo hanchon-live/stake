aad2cdf342de2a1fa714118a14acf7595f7f9b50
hanchon <guillermo.paoletti@gmail.com>
2024-01-26 00:56:36 +0100

    feat: use echo as html server

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/a-h/templ"
+	"github.com/gorilla/sessions"
 	"github.com/hanchon-live/stake/src/components"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+const (
+	secret_key string = "cookie-key"
 )
 
 func main() {
-	component := components.Body()
+	server := echo.New()
+	server.Static("/public/assets/", "./public/assets/")
 
-	http.Handle("/", templ.Handler(component))
+	server.Use(middleware.Logger())
+	server.Use(session.Middleware(sessions.NewCookieStore([]byte(secret_key))))
 
-	fs := http.FileServer(http.Dir("./public/assets"))
-	http.Handle("/public/assets/", http.StripPrefix("/public/assets/", fs))
+	server.GET("/", func(c echo.Context) error {
+		component := components.Body()
+		return component.Render(c.Request().Context(), c.Response().Writer)
+	})
 
-	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	server.Logger.Fatal(server.Start(":3000"))
 }
